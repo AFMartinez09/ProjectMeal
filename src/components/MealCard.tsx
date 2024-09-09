8cc79c071cfa5d56c9833da486c27172a6277c03
Andres Martinez <andresfelipemartinezt@gmail.com>
2024-09-09 18:53:58 -0500

    creating structure (components)

---
+++ src/components/MealCard.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+type Props = {}
+
+const MealCard = (props: Props) => {
+  return (
+    <div>MealCard</div>
+  )
+}
+
+export default MealCard
---
+++ src/components/RecipeModal.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+type Props = {}
+
+const RecipeModal = (props: Props) => {
+  return (
+    <div>RecipeModal</div>
+  )
+}
+
+export default RecipeModal

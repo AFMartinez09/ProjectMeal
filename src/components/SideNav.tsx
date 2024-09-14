851835808e152adf5f0c5443c5de7b9b53e53398
Andres Martinez <andresfelipemartinezt@gmail.com>
2024-09-13 20:14:58 -0500

    Creating logical and style to show what option the user clicked in categories

--- src/components/SideNav.tsx
+++ src/components/SideNav.tsx
@@ -1,11 +1,45 @@
-import React from 'react'
+import { Heading, Link, VStack } from "@chakra-ui/react";
+import { Category } from "../type";
 
-type Props = {}
+type Props = {
+  categories: Category[];
+  loading: boolean;
+  selectedCategory: Category;
+  // recieve parameter (type category)
+  setSelectedCategory: (category: Category) => void;
+};
 
-const SideNav = (props: Props) => {
+const selectedProps = {
+  bgColor: "blue.600",
+  color: "white",
+  fontWeight: "bold",
+};
+
+const SideNav = ({
+  categories,
+  selectedCategory,
+  setSelectedCategory,
+}: Props) => {
   return (
-    <div>SideNav</div>
-  )
-}
+    <>
+      <Heading color="blue.700" fontSize={12} fontWeight="bold" mb={4}>
+        CATEGORIES
+      </Heading>
+      <VStack align="stretch">
+        {categories.map((c) => (
+          <Link
+            onClick={() => setSelectedCategory(c)}
+            key={c.strCategory}
+            px={2}
+            py={1}
+            borderRadius={5}
+            {...(selectedCategory.strCategory == c.strCategory && selectedProps)}
+          >{c.strCategory}
+          </Link>
+        ))}
+      </VStack>
+    </>
+  );
+};
 
-export default SideNav
+export default SideNav;

7bbd043d29177a85b47c9101ebc6e7954d42888a
Andres Martinez <andresfelipemartinezt@gmail.com>
2024-09-14 20:55:51 -0500

    refactoring App.tsx code to create useHttpData

--- src/App.tsx
+++ src/App.tsx
@@ -1,44 +1,18 @@
-import { useEffect, useState } from "react";
+import { useState } from "react";
 import { Grid, GridItem } from "@chakra-ui/react";
-import axios from "axios";
 import "./App.css";
 import Header from "./components/Header";
 import SideNav from "./components/SideNav";
 import MainContent from "./components/MainContent";
-import { CategoriesResponse, Category } from "./type";
+import { Category } from "./type";
+import useHttpData from "./hooks/useHttpData";
 
 function App() {
   const url = "https://www.themealdb.com/api/json/v1/1/list.php?c=list";
   const [selectedCategory, setSelectedCategory] = useState<Category>({
     strCategory: "Beef",
   });
-  const [ data, setData ] = useState<Category[]>([]);
-  const [ loading, setLoading] = useState(false);
-  
-  useEffect (() => {
-    //  ignore help us to avoid rerender (twice) our component 
-    let ignore = false;
-    // controller allow us to call once not twice (data API)
-    const controller = new AbortController();
-    const { signal } = controller;
-    setLoading(true);
-
-    axios
-      .get<CategoriesResponse>(url, { signal })
-      .then(({ data }) => {
-        if(!ignore){
-          setData(data.meals)
-        }})
-      .finally(() => {
-        if(!ignore){
-          setLoading(false)
-        }
-      });
-      return () => {{
-        ignore = true;
-        controller.abort();
-      }}
-  },[]);
+  const { loading, data } = useHttpData<Category>(url);
   return (
     <Grid
       templateAreas={`"header header"

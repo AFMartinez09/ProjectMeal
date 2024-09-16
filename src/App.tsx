ae18aacd45b8e7609719e67683c79ef458e6a126
Andres Martinez <andresfelipemartinezt@gmail.com>
2024-09-16 16:03:16 -0500

    adding style in GidItem (SideNav and Header) adding position sticky, changing bg-color, and z-index(Header)

--- src/App.tsx
+++ src/App.tsx
@@ -9,21 +9,23 @@ import useHttpData from "./hooks/useHttpData";
 
 const url = "https://www.themealdb.com/api/json/v1/1/list.php?c=list";
 
-const makeMealUrl = ( category: Category) => 
+const makeMealUrl = (category: Category) =>
   `https://www.themealdb.com/api/json/v1/1/filter.php?c=${category.strCategory}`;
-
 
 const defaultCategory = {
   strCategory: "Beef",
-}
+};
 
 function App() {
-  const [selectedCategory, setSelectedCategory] = useState<Category>(defaultCategory);
+  const [selectedCategory, setSelectedCategory] =
+    useState<Category>(defaultCategory);
   const { loading, data } = useHttpData<Category>(url);
-  const { loading: loadingMeal, data: dataMeal } = useHttpData<Meal>(makeMealUrl(defaultCategory));
-  
+  const { loading: loadingMeal, data: dataMeal } = useHttpData<Meal>(
+    makeMealUrl(defaultCategory)
+  );
+
   console.log({ dataMeal });
-  
+
   return (
     <Grid
       templateAreas={`"header header"
@@ -34,19 +36,27 @@ function App() {
       gridTemplateColumns={{ sm: `0 1fr`, md: `250px 1fr` }}
       fontSize={14}
     >
-      <GridItem pl="2" bg="orange.300" area={"header"}>
+      <GridItem zIndex="1" pos="sticky" top="0" pl="2" bg="gray.300" area={"header"}>
         <Header />
       </GridItem>
-      <GridItem p="5" bg="gray.100" area={"nav"} height="calc(100vh - 60px)">
-        <SideNav 
-        categories = {data} 
-        loading={loading}
-        selectedCategory = {selectedCategory}
-        setSelectedCategory = {setSelectedCategory}
+      <GridItem
+        pos="sticky"
+        top="60px"
+        p="5"
+        bg="gray.100"
+        area={"nav"}
+        height="calc(100vh - 60px)"
+        overflowY="auto"
+      >
+        <SideNav
+          categories={data}
+          loading={loading}
+          selectedCategory={selectedCategory}
+          setSelectedCategory={setSelectedCategory}
         />
       </GridItem>
-      <GridItem pl="2" bg="green.300" area={"main"}>
-        <MainContent />
+      <GridItem pl="4" bg="gray.50" area={"main"}>
+        <MainContent loading={loadingMeal} meals={dataMeal} />
       </GridItem>
     </Grid>
   );

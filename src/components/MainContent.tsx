36c01af7c6dc664d7e3d70c6606cbaef350e3a73
Andres Martinez <andresfelipemartinezt@gmail.com>
2024-09-16 16:01:03 -0500

    creating card dynamically (logical and styles

--- src/components/MainContent.tsx
+++ src/components/MainContent.tsx
@@ -1,11 +1,41 @@
-import React from 'react'
+import { Meal } from "../type";
+import {
+  Button,
+  Card,
+  CardBody,
+  Heading,
+  Image,
+  SimpleGrid,
+  Stack,
+} from "@chakra-ui/react";
 
-type Props = {}
+type Props = {
+  meals: Meal[];
+  loading: boolean;
+};
 
-const MainContent = (props: Props) => {
+const MainContent = ({ meals, loading }: Props) => {
   return (
-    <div>MainContent</div>
-  )
-}
+    <SimpleGrid columns={[2, null, 3]} spacing="20px">
+      {meals.map(m => (
+              <Card key={m.idMeal} maxW="xs" m="2">
+              <CardBody>
+                <Image
+                  src={m.strMealThumb}
+                  alt={m.strMeal}
+                  borderRadius="lg"
+                />
+                <Stack margin="3" spacing="3">
+                  <Heading size="md">{m.strMeal}</Heading>
+                </Stack>
+              </CardBody>
+                <Button bg="blue.600" color="whitesmoke" m={2}>
+                  SHOW RECIPE
+                </Button>
+            </Card>
+      ))}
+    </SimpleGrid>
+  );
+};
 
-export default MainContent
+export default MainContent;

50799a270029ca09736bcf32e6f0d6f47dcec914
Andres Martinez <andresfelipemartinezt@gmail.com>
2024-09-16 15:07:13 -0500

    creating type to find meals

--- src/type/index.ts
+++ src/type/index.ts
@@ -2,3 +2,9 @@ export type Category = {
   // La respuesta cuando se llaman todos los platos
   strCategory: string;
 }
+
+export type Meal = {
+  strMeal: string;
+  strMealThumb: string;
+  idMeal: string; 
+}

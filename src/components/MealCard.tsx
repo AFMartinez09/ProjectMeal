6486ab4eba5161705217d772769ba3295bfa9382
Andres Martinez <andresfelipemartinezt@gmail.com>
2024-09-17 16:46:48 -0500

    Creating component MealCard (moving logical from MainContent to MealCard.tsx)

--- src/components/MealCard.tsx
+++ src/components/MealCard.tsx
@@ -1,10 +1,37 @@
-import React from 'react'
+import {
+  Button,
+  Card,
+  CardBody,
+  Heading,
+  Image,
+  Stack,
+  Text
+} from "@chakra-ui/react";
+import { Meal } from '../type';
 
-type Props = {}
+type Props = {
+  meal: Meal;
+}
 
-const MealCard = (props: Props) => {
+const MealCard = ({ meal }: Props) => {
   return (
-    <div>MealCard</div>
+    <Card maxW="md" m="2">
+    <CardBody>
+      <Image
+        src={meal.strMealThumb}
+        alt={meal.strMeal}
+        borderRadius="lg"
+      />
+      <Stack margin="3" spacing="3">
+        <Heading size="md">
+          <Text>{meal.strMeal}</Text>
+        </Heading>
+      </Stack>
+    </CardBody>
+      <Button bg="blue.600" color="whitesmoke" m={2}>
+        SHOW RECIPE
+      </Button>
+  </Card>
   )
 }
 

df94bd341ee8577a6acd6db927b05a0a35a79ebf
Andres Martinez <andresfelipemartinezt@gmail.com>
2024-09-18 16:06:44 -0500

    finishing Header.tsx component (validate data, styles, and formState)

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,19 +1,39 @@
-import { Container, Input, InputGroup, InputLeftElement } from "@chakra-ui/react"
+import {
+  Button,
+  Container,
+  Input,
+  InputGroup,
+  InputLeftElement,
+} from "@chakra-ui/react";
+import { useForm } from "react-hook-form";
 import { IoSearch } from "react-icons/io5";
+import { SearchForm } from "../type";
 
-type Props = {}
+type Props = {
+  onSubmit: (data: SearchForm) => void;
+};
 
-const Header = ({}: Props) => {
+const Header = ({ onSubmit }: Props) => {
+  const { register, formState, handleSubmit } = useForm<SearchForm>();
   return (
-  <Container maxW="3xl" mt="1" >
-    <InputGroup borderColor="gray.600">
-      <InputLeftElement pointerEvents='none'>
-      <IoSearch color="gray.600"/>
-      </InputLeftElement >
-      <Input type='text' placeholder='Intenta con "chicken" ó "beans"' />
-    </InputGroup>
-  </Container>
-  )
-}
+    <Container maxW="3xl" mt="1">
+      <form onSubmit={handleSubmit(onSubmit)}>
+        <InputGroup borderColor="gray.600">
+          <InputLeftElement pointerEvents="none">
+            <IoSearch color="gray.600" />
+          </InputLeftElement>
+          <Input
+          focusBorderColor={!!formState.errors.search ? "crimson" : "blue.400"}
+            isInvalid= {!!formState.errors.search}
+            {...register("search", { required: true })}
+            type="text"
+            placeholder='Intenta con "chicken" ó "beans"'
+          />
+          <Button type="submit" bgColor="blue.600" color="whitesmoke" ml={1}>Buscar</Button>
+        </InputGroup>
+      </form>
+    </Container>
+  );
+};
 
-export default Header
+export default Header;

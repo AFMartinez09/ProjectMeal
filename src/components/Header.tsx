59e7a791d025814aca2f14e91d36ed65e22d5209
Andres Martinez <andresfelipemartinezt@gmail.com>
2024-09-18 14:59:44 -0500

    creating input and adding icon in searchBar (Header.tsx)

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,10 +1,18 @@
-import React from 'react'
+import { Container, Input, InputGroup, InputLeftElement } from "@chakra-ui/react"
+import { IoSearch } from "react-icons/io5";
 
 type Props = {}
 
-const Header = (props: Props) => {
+const Header = ({}: Props) => {
   return (
-    <div>Header</div>
+  <Container maxW="3xl" mt="1" >
+    <InputGroup borderColor="gray.600">
+      <InputLeftElement pointerEvents='none'>
+      <IoSearch color="gray.600"/>
+      </InputLeftElement >
+      <Input type='text' placeholder='Intenta con "chicken" ó "beans"' />
+    </InputGroup>
+  </Container>
   )
 }
 
